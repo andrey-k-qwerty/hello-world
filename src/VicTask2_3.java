343dfc78f89252a503de7d75dfc141f48247b3af
andrey-k-qwerty <andrey.k.qwerty@gmail.com>
2018-10-31 22:23:20 +0200

    забыл)

--- src/VicTask2_3.java
+++ src/VicTask2_3.java
@@ -1,3 +1,4 @@
+
 /*Консольная программа.
 Предыдущая задача. Только результат вывести в файл (можно захардкодить имя и путь к файлу).
 Формат вывода - по 10 пар "слово количествоПовторений" в каждой строке.
@@ -11,65 +12,59 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-import com.sun.org.apache.xml.internal.serialize.Printer;
-
 public class VicTask2_3 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException
+	{
 		String dataLine = "один, два.три;четыре!пять?шесть-семь восемь;один,, два....три;четыре!?пять?-семь восемь";
+		// Соз
 		FilterLineReader filterLineReader = new FilterLineReader(new StringReader(dataLine));
-	
-		filterLineReader.regex = "[\\.,;!\\?-]" ; 
+
+		filterLineReader.regex = "[\\.,;!\\?-]";
 		filterLineReader.replacement = " ";
-		 
-		 String currentLine;
-		 int maxLenghtWord = 0;
-		 TreeMap<String, Integer> treeMap = new TreeMap<>();
-		while (( currentLine = filterLineReader.readLine()) != null) 
-			{
+
+		String currentLine;
+		int maxLenghtWord = 0;
+		TreeMap<String, Integer> treeMap = new TreeMap<>();
+		while ((currentLine = filterLineReader.readLine()) != null)
+		{
 			Scanner scanner = new Scanner(currentLine);
-			
+
 			while (scanner.hasNext())
 			{
-				 String  currentWorld = scanner.next();
-				 maxLenghtWord = Math.max(currentWorld.length(), maxLenghtWord);
-				 Integer i = treeMap.get(currentWorld);
-				 treeMap.put(currentWorld, i == null ? 1 : ++i) ;
+				String currentWorld = scanner.next();
+				maxLenghtWord = Math.max(currentWorld.length(), maxLenghtWord);
+				Integer i = treeMap.get(currentWorld);
+				treeMap.put(currentWorld, i == null ? 1 : ++i);
 			}
 			scanner.close();
-			
-		    }  
-		  
+
+		}
+
 		filterLineReader.close();
-		
-		
-	//	System.out.println(treeMap.toString());
+
+		// System.out.println(treeMap.toString());
 		int i = 1;
 		StringBuilder sb = new StringBuilder();
-		for(Map.Entry<String,Integer> entry : treeMap.entrySet()) {
-			  String key = entry.getKey();
-			  Integer value = entry.getValue();
-			  sb.append(String.format("%"+(maxLenghtWord + 1) +"s %d", key, value));
-               if (i%3 == 0)
-            	   sb.append("\n");
-		
-             i++;
-			}
-		  System.out.println(sb.toString());	
-		  
-		 PrintWriter printWriter = new PrintWriter("out.txt"); 
-		 printWriter.print(sb.toString());
-		 printWriter.close();
+		for (Map.Entry<String, Integer> entry : treeMap.entrySet())
+		{
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			sb.append(String.format("%" + (maxLenghtWord + 1) + "s %d", key, value));
+			if (i % 2 == 0)
+				sb.append("\n");
+
+			i++;
 		}
-	
-       
+		System.out.println(sb.toString());
+
+		PrintWriter printWriter = new PrintWriter("out.txt");
+		printWriter.print(sb.toString());
+		printWriter.close();
 	}
 
-
-
-
+}
