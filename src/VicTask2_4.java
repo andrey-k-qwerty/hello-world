a431931823b485ec7254bffdad9761c512482db0
Andrey Kuzmenko <a.kuzmenko@ossystem.com.ua>
2018-11-01 17:37:14 +0200

    update2

--- src/VicTask2_4.java
+++ src/VicTask2_4.java
@@ -12,6 +12,9 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 public class VicTask2_4 {
+	/**
+	 * Метод возвращает истину если в слове больше английских гласных 
+	 * */
 	public static boolean isMoreVowels(String src)
 	{
 		int lenghtWord = src.length();
@@ -22,11 +25,11 @@ public class VicTask2_4 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		String dataLine = "one, two.three;four!five?six-seven eight --- foo;one,, two....three;four!?five?-seven-foo- eight;;ten";
+		String dataLine = "one,,dd, two.three;four!five?six-seven eight --- foo;one,,dd two....three;four!?five?-seven-foo- eight;;ten";
 		
 		Scanner scannerDataLine = new Scanner(dataLine);
 		//Для файла
-		//Scanner scanerFile = new Scanner(Paths.get("myfile.txt")); 
+		//Scanner scannerDataLine = new Scanner(Paths.get("myfile.txt")); 
 		
 		scannerDataLine.useDelimiter("[\\.,;!\\?\\s-]+");
 				
@@ -53,16 +56,19 @@ public class VicTask2_4 {
 			Integer value = entry.getValue();
 			if (key.length() > 2 && !isMoreVowels(key))
 			{	
-			sb.append(String.format("%" + (maxLenghtWord + 1) + "s %"+ countDigit + "d", key, value));
+			sb.append(String.format("%" + (maxLenghtWord) + "s %"+ countDigit + "d", key, value));
 			 
-			if (i % 10 == 0)
+			if (i % 3 == 0)
 				sb.append("\n");
-
+			else
+				sb.append("|");
 			i++;
 			}
 		}
+		if (sb.charAt(sb.length()-1) == '|') sb.deleteCharAt(sb.length()-1);
 		System.out.println(sb.toString());
-
+		
+		System.out.println("Сохраняем в файл");
 		PrintWriter printWriter = new PrintWriter("out.txt");
 		printWriter.print(sb.toString());
 		printWriter.close();
